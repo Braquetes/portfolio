8af02d8c221a412f5793056106d5923b21f0e478
braquetes <braquetesss@gmail.com>
2024-01-18 03:10:52 -0600

    home letras css

--- src/app/pages/client/client.module.ts
+++ src/app/pages/client/client.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
+import { HomeComponent } from './home/home.component';
 import { BioComponent } from './bio/bio.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -11,12 +12,13 @@ import { CvComponent } from './components/cv/cv.component';
 
 @NgModule({
     declarations: [
+        HomeComponent,
         BioComponent,
         FooterComponent,
         NavbarComponent,
         CertificatesComponent,
         ProjectsComponent,
-        CvComponent
+        CvComponent,
     ],
     imports: [
         CommonModule,
